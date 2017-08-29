5c5f0c9e019e19b2f82fc158d15d32f305d4d736
pauvert louis <louis.pauvert@viacesi.fr>
2017-08-29 12:09:14 +0200

    29/08/2017

---
+++ src/main/java/com/example/cesi/seating_plan/dao/implement/MaterielDAO.java
@@ -0,0 +1,26 @@
+package com.example.cesi.seating_plan.dao.implement;
+
+import com.example.cesi.seating_plan.dao.DAO;
+import com.example.cesi.seating_plan.model.Materiel;
+
+public class MaterielDAO extends DAO<Materiel> {
+    @Override
+    public boolean create(Materiel obj) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Materiel obj) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Materiel obj) {
+        return false;
+    }
+
+    @Override
+    public Materiel find(long id) {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/cesi/seating_plan/model/Materiel.java
@@ -0,0 +1,57 @@
+package com.example.cesi.seating_plan.model;
+
+public class Materiel {
+    private long id;
+
+    private String libelle;
+
+    private int nombre;
+
+    private long id_type;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public Materiel(String libelle, int nombre, long id_type) {
+        this.libelle = libelle;
+        this.nombre = nombre;
+        this.id_type = id_type;
+    }
+
+    public Materiel(long id, String libelle, int nombre, long id_type) {
+
+        this.id = id;
+        this.libelle = libelle;
+        this.nombre = nombre;
+        this.id_type = id_type;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getId_type() {
+        return id_type;
+    }
+
+    public void setId_type(long id_type) {
+        this.id_type = id_type;
+    }
+}
