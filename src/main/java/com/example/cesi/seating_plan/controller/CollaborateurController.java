61cd1ef4b33a6433ae036ab6bcd7d4777a3cfcf4
pauvert louis <louis.pauvert@viacesi.fr>
2017-08-29 17:27:57 +0200

    29/08/2017

--- src/main/java/com/example/cesi/seating_plan/controller/CollaborateurController.java
+++ src/main/java/com/example/cesi/seating_plan/controller/CollaborateurController.java
@@ -6,14 +6,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ListIterator;
+
 @Controller
 @RequestMapping("/collaborateur")
 public class CollaborateurController {
     CollaborateurDAO collaborateurDAO = new CollaborateurDAO();
 
+
     @RequestMapping(value = "/id/{id}", method = RequestMethod.GET)
     public String findOne(@PathVariable("id") Long id){
 
         return collaborateurDAO.find(id).toString();
+    }
+
+    @RequestMapping(value = "/id/all", method = RequestMethod.GET)
+    public String findAll(){
+        ListIterator listIterator = collaborateurDAO.findall().listIterator();
+
+        String liste = "";
+
+        while (listIterator.hasNext()){
+            liste += listIterator.next().toString();
+        }
+        return  liste;
     }
 }
--- src/main/java/com/example/cesi/seating_plan/dao/implement/CollaborateurDAO.java
+++ src/main/java/com/example/cesi/seating_plan/dao/implement/CollaborateurDAO.java
@@ -9,6 +9,8 @@ import javax.transaction.Transactional;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -16,19 +18,46 @@ public class CollaborateurDAO extends DAO<Collaborateur> {
 
     @Override
     public boolean create(Collaborateur obj) {
+        try {
+            ResultSet result = this.connect.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY).executeQuery("INSERT INTO collaborateur (nom, prenom, date_arrive, date_depart, email, mdp) VALUES ("+obj.getNom()+", "+obj.getPrenom()+", "+obj.getDate_arrive()+", "+obj.getDate_depart()+", "+obj.getEmail()+", "+obj.getMdp()+")") ;
 
+            return result.rowInserted();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public boolean delete(Collaborateur obj) {
+        try {
+            ResultSet result = this.connect.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY).executeQuery("DELETE FROM collaborateur WHERE id = " + String.valueOf(obj.getId()));
 
+            return result.rowInserted();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public boolean update(Collaborateur obj) {
+        try {
+            ResultSet result = this.connect.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY).executeQuery("UPDATE collaborateur SET nom = "+obj.getNom()+", prenom = "+obj.getPrenom()+", date_arrive = "+obj.getDate_arrive()+", date_depart = "+ obj.getDate_depart()+", email = "+ obj.getEmail()+", mdp = "+ obj.getMdp()) ;
 
+            return result.rowUpdated();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
@@ -39,7 +68,7 @@ public class CollaborateurDAO extends DAO<Collaborateur> {
         try {
             ResultSet result = this.connect.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
-                    ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM Collaborateur WHERE id = " + String.valueOf(id));
+                    ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM collaborateur WHERE id = " + String.valueOf(id));
             if(result.first())
                 collaborateur = new Collaborateur(
                         id,
@@ -54,5 +83,32 @@ public class CollaborateurDAO extends DAO<Collaborateur> {
             e.printStackTrace();
         }
         return collaborateur;
+    }
+    public List<Collaborateur> findall() {
+        List<Collaborateur> collaborateurList = new LinkedList<>();
+
+        Collaborateur collaborateur = new Collaborateur("","","");
+
+        try {
+            ResultSet result = this.connect.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM collaborateur");
+            while (result.next()) {
+                collaborateur = new Collaborateur(
+                        result.getLong("id"),
+                        result.getString("nom"),
+                        result.getString("prenom"),
+                        result.getDate("date_arrive"),
+                        result.getDate("date_depart"),
+                        result.getString("email"),
+                        result.getString("mdp")
+                );
+
+                collaborateurList.add(collaborateur);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return collaborateurList;
     }
 }
